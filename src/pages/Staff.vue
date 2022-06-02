9832109f7bfca0dcb2880c2aa9e1f46d87976085
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-02 06:16:46 +0100

    chore: staff page

---
+++ src/pages/Staff.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page class="q-pa-lg text-center">
+    <h5 class="q-mt-none q-mb-sm">School Staffs</h5>
+    <div class="row">
+      <div class="col-6 q-pa-md" v-for="(user, key) in allUsers" :key="user.id">
+        <div
+          v-if="user.select === 'Staff' || user.select === 'Non-Teaching-Staff'"
+        >
+          <q-avatar size="80px">
+            <q-img
+              :loading="loading"
+              :src="user.image"
+              :placeholder-src="placeHolder"
+            />
+          </q-avatar>
+          <div>
+            <h6 class="text-h6 q-my-none text-grey">
+              {{ user.name }} is a
+              <q-badge :color="user.select === 'staff' ? 'primary' : 'accent'">
+                {{ user.select }}
+              </q-badge>
+            </h6>
+            <p class="q-my-none">
+              Department of:
+              <q-badge color="negative" :label="user.department" />
+            </p>
+            <p>
+              {{ user.email }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "staffs",
+  data: () => ({
+    placeHolder: "https://cdn.quasar.dev/img/boy-avatar.png",
+    loading: false
+  }),
+  mounted() {},
+  computed: {
+    ...mapState("store", ["allUsers", "userDetails"])
+  }
+};
+</script>
+
+<style>
+</style>
